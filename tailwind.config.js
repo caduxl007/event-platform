5e3e5169f83906f92be03e8cf7994e0cfe0d2ce4
caduxl007 <eduardo2020dev@gmail.com>
2022-06-23 20:30:05 -0300

    feat: create page Subscribe

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,9 @@ module.exports = {
   ],
   theme: {
     extend: {
+      backgroundImage: {
+        blue: 'url(/src/assets/blur-background.png)'
+      },
       fontFamily: {
         sans: 'Roboto, sans-serif',
       },
